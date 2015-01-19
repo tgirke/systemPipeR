c87f9727b22a6a985d2fb00bdb91aac604c28b8e
t.girke <t.girke@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-01-19 23:20:35 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: d4e84d2e2d6194a72a21029d9c81d6d1d4640b43
    
        some edits
    
    Committed by: Thomas Girke
    Author Name: Thomas Girke
    Commit date: 2015-01-19 15:20:33 -0800
    Author date: 2015-01-19 15:20:33 -0800
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/systemPipeR@98475 bc3139a8-67e5-0310-9ffc-ced21a209358

--- vignettes/systemPipeR.R
+++ vignettes/systemPipeR.R
@@ -153,21 +153,42 @@ read.table(system.file("extdata", "alignStats.xls", package="systemPipeR"), head
 
 
 ###################################################
-### code chunk number 22: systemPipeR.Rnw:207-209 (eval = FALSE)
+### code chunk number 22: systemPipeR.Rnw:209-212 (eval = FALSE)
 ###################################################
 ## args <- systemArgs(sysma="bowtieSE.param", mytargets="targets.txt")
+## moduleload(modules(args)) # Skip if module system is not available
 ## bampaths <- runCommandline(args=args)
 
 
 ###################################################
-### code chunk number 23: systemPipeR.Rnw:213-215 (eval = FALSE)
+### code chunk number 23: systemPipeR.Rnw:216-218 (eval = FALSE)
 ###################################################
 ## qsubargs <- getQsubargs(queue="batch", cores=cores(args), memory="mem=10gb", time="walltime=20:00:00")
 ## (joblist <- qsubRun(args=args, qsubargs=qsubargs, Nqsubs=18, package="systemPipeR"))
 
 
 ###################################################
-### code chunk number 24: systemPipeR.Rnw:220-223 (eval = FALSE)
+### code chunk number 24: systemPipeR.Rnw:223-227 (eval = FALSE)
+###################################################
+## args <- systemArgs(sysma="bwa.param", mytargets="targets.txt")
+## moduleload(modules(args)) # Skip if module system is not available
+## system("bwa index -a bwtsw ./data/tair10.fasta") # Indexes reference genome
+## bampaths <- runCommandline(args=args)
+
+
+###################################################
+### code chunk number 25: systemPipeR.Rnw:233-239 (eval = FALSE)
+###################################################
+## library(Rsubread)
+## args <- systemArgs(sysma="rsubread.param", mytargets="targets.txt")
+## buildindex(basename=reference(args), reference=reference(args)) # Build indexed reference genome
+## align(index=reference(args), readfile1=infile1(args), input_format="FASTQ", 
+##       output_file=outfile1(args), output_format="SAM", nthreads=8, indels=1, TH1=2)
+## for(i in seq(along=outfile1(args))) asBam(file=outfile1(args)[i], destination=gsub(".sam", "", outfile1(args)[i]), overwrite=TRUE, indexDestination=TRUE)
+
+
+###################################################
+### code chunk number 26: systemPipeR.Rnw:244-247 (eval = FALSE)
 ###################################################
 ## library(GenomicFeatures)
 ## txdb <- makeTranscriptDbFromGFF(file="data/tair10.gff", format="gff", dataSource="TAIR", species="A. thaliana")
@@ -175,7 +196,7 @@ read.table(system.file("extdata", "alignStats.xls", package="systemPipeR"), head
 
 
 ###################################################
-### code chunk number 25: systemPipeR.Rnw:227-238 (eval = FALSE)
+### code chunk number 27: systemPipeR.Rnw:251-262 (eval = FALSE)
 ###################################################
 ## library(BiocParallel)
 ## txdb <- loadDb("./data/tair10.sqlite")
@@ -191,7 +212,7 @@ read.table(system.file("extdata", "alignStats.xls", package="systemPipeR"), head
 
 
 ###################################################
-### code chunk number 26: systemPipeR.Rnw:243-252 (eval = FALSE)
+### code chunk number 28: systemPipeR.Rnw:267-276 (eval = FALSE)
 ###################################################
 ## system("wget ftp://mirbase.org/pub/mirbase/19/genomes/My_species.gff3 -P ./data/")
 ## gff <- import.gff("./data/My_species.gff3", asRangedData=FALSE)
@@ -205,7 +226,7 @@ read.table(system.file("extdata", "alignStats.xls", package="systemPipeR"), head
 
 
 ###################################################
-### code chunk number 27: sample_tree
+### code chunk number 29: sample_tree
 ###################################################
 library(ape)
 rpkmDFeBygpath <- system.file("extdata", "rpkmDFeByg.xls", package="systemPipeR")
@@ -217,7 +238,7 @@ plot.phylo(as.phylo(hc), type="p", edge.col="blue", edge.width=2, show.node.labe
 
 
 ###################################################
-### code chunk number 28: systemPipeR.Rnw:274-277
+### code chunk number 30: systemPipeR.Rnw:298-301
 ###################################################
 targets <- read.delim(targetspath, comment="#")
 cmp <- readComp(file=targetspath, format="matrix", delim="-")
@@ -225,7 +246,7 @@ cmp[[1]]
 
 
 ###################################################
-### code chunk number 29: systemPipeR.Rnw:280-283
+### code chunk number 31: systemPipeR.Rnw:304-307
 ###################################################
 countDFeBygpath <- system.file("extdata", "countDFeByg.xls", package="systemPipeR")
 countDFeByg <- read.delim(countDFeBygpath, row.names=1)
@@ -233,20 +254,20 @@ edgeDF <- run_edgeR(countDF=countDFeByg, targets=targets, cmp=cmp[[1]], independ
 
 
 ###################################################
-### code chunk number 30: DEGcounts
+### code chunk number 32: DEGcounts
 ###################################################
 DEG_list <- filterDEGs(degDF=edgeDF, filter=c(Fold=2, FDR=10))
 
 
 ###################################################
-### code chunk number 31: systemPipeR.Rnw:295-297
+### code chunk number 33: systemPipeR.Rnw:319-321
 ###################################################
 names(DEG_list)
 DEG_list$Summary[1:4,]
 
 
 ###################################################
-### code chunk number 32: vennplot
+### code chunk number 34: vennplot
 ###################################################
 vennsetup <- overLapper(DEG_list$Up[6:9], type="vennsets")
 vennsetdown <- overLapper(DEG_list$Down[6:9], type="vennsets")
@@ -254,7 +275,7 @@ vennPlot(list(vennsetup, vennsetdown), mymain="", mysub="", colmode=2, ccol=c("b
 
 
 ###################################################
-### code chunk number 33: systemPipeR.Rnw:317-328 (eval = FALSE)
+### code chunk number 35: systemPipeR.Rnw:341-352 (eval = FALSE)
 ###################################################
 ## library("biomaRt")
 ## listMarts() # To choose BioMart database
@@ -270,7 +291,7 @@ vennPlot(list(vennsetup, vennsetdown), mymain="", mysub="", colmode=2, ccol=c("b
 
 
 ###################################################
-### code chunk number 34: systemPipeR.Rnw:333-344 (eval = FALSE)
+### code chunk number 36: systemPipeR.Rnw:357-368 (eval = FALSE)
 ###################################################
 ## load("data/GO/catdb.RData")
 ## DEG_list <- filterDEGs(degDF=edgeDF, filter=c(Fold=2, FDR=50), plot=FALSE)
@@ -286,7 +307,7 @@ vennPlot(list(vennsetup, vennsetdown), mymain="", mysub="", colmode=2, ccol=c("b
 
 
 ###################################################
-### code chunk number 35: systemPipeR.Rnw:349-354 (eval = FALSE)
+### code chunk number 37: systemPipeR.Rnw:373-378 (eval = FALSE)
 ###################################################
 ## gos <- BatchResultslim[grep("M6-V6_up_down", BatchResultslim$CLID), ]
 ## gos <- BatchResultslim
@@ -296,7 +317,7 @@ vennPlot(list(vennsetup, vennsetdown), mymain="", mysub="", colmode=2, ccol=c("b
 
 
 ###################################################
-### code chunk number 36: systemPipeR.Rnw:367-373 (eval = FALSE)
+### code chunk number 38: systemPipeR.Rnw:391-397 (eval = FALSE)
 ###################################################
 ## library(pheatmap)
 ## geneids <- unique(as.character(unlist(DEG_list[[1]])))
@@ -307,7 +328,7 @@ vennPlot(list(vennsetup, vennsetdown), mymain="", mysub="", colmode=2, ccol=c("b
 
 
 ###################################################
-### code chunk number 37: sessionInfo
+### code chunk number 39: sessionInfo
 ###################################################
 toLatex(sessionInfo())
 
