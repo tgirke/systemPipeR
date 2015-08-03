95ad6eff680eaa12a70f00ac813a266f3e6e5dcd
Thomas Girke <thomas.girke@ucr.edu>
2015-08-03 22:52:13 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 8badd8dffd52a3a3c0ba453d14f0a481e9e1e057
    
        some edits
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/systemPipeR@107041 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/extdata/slides/systemPipeRslides.R
@@ -0,0 +1,28 @@
+## ----install, eval=FALSE-------------------------------------------------
+## source("http://bioconductor.org/biocLite.R")
+## biocLite("systemPipeR")
+## biocLite("tgirke/systemPipeRdata", build_vignettes=TRUE, dependencies=TRUE) # github
+
+## ----load_systemPipeR_hidden, eval=TRUE, include=FALSE, messages=FALSE, warnings=FALSE, cache=TRUE----
+library("systemPipeR"); library("systemPipeRdata")
+
+## ----load_systemPipeR_print, eval=FALSE, messages=FALSE, warnings=FALSE, cache=TRUE----
+## library("systemPipeR"); library("systemPipeRdata")
+
+## ----show_targets, eval=TRUE, messages=FALSE, warnings=FALSE, cache=TRUE----
+targetspath <- system.file("extdata", "targets.txt", package="systemPipeR")
+read.delim(targetspath, comment.char = "#")[1:4,1:5]
+
+## ----generate_workenvir, eval=FALSE, cache=TRUE--------------------------
+## ### <b>
+## genWorkenvir(workflow="varseq", mydirname=NULL)
+## ### </b>
+## setwd("varseq")
+
+## ----generate_workenvir_from_shell, eval=FALSE, cache=TRUE, engine="sh"----
+## $ echo 'library(systemPipeRdata);
+##         genWorkenvir(workflow="varseq", mydirname=NULL)' | R --slave
+
+## ----run_make, eval=FALSE, engine="sh"-----------------------------------
+## $ make -B
+
