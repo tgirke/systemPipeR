bbcc94bf11bf6c1e219e95cf5082b2f6b7d36552
unknown <unknown>
2015-08-04 04:52:22 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 9f74252628d25c6b9dde3aeeaae6e1824b125926
    
        some edits
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/systemPipeR@107062 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extdata/slides/systemPipeRslides.R
+++ inst/extdata/slides/systemPipeRslides.R
@@ -24,7 +24,7 @@ read.delim(targetspath, comment.char = "#")[1:4,1:5]
 ##         genWorkenvir(workflow="varseq", mydirname=NULL)' | R --slave
 
 ## ----workflow_template_structure, eval=FALSE-----------------------------
-## workflow_name/            # *.Rnw/*.Rmd scripts and targets file
+## workflow_name/            # *.Rnw/*.Rmd scripts, targets file, etc.
 ##                 param/    # parameter files for command-line software
 ##                 data/     # inputs e.g. FASTQ, reference, annotations
 ##                 results/  # analysis result files
