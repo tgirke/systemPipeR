dfc8ac91c3b331e5599d924424a50332c683e08c
dcassol <danicassol@gmail.com>
2019-10-15 13:29:16 -0700

    loadWF

--- R/AllClasses2.R
+++ R/AllClasses2.R
@@ -259,9 +259,7 @@ loadWorkflow <- function(targets=NULL, wf_file, input_file, dir_path=".") {
 }
 
 ## Wrapper for loadWorkflow: Short and consistent name for the function
-loadWF <- function(targets=NULL, wf_file, input_file, dir_path="."){
-  loadWorkflow(targets=targets, wf_file, input_file, dir_path=dir_path)
-}
+loadWF <- loadWorkflow
 
 ## Usage:
 # targets <- system.file("extdata", "targets.txt", package="systemPipeR")
