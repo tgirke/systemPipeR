2da0ce4c6512592f426cb4f605ce8475540500bb
dcassol <danicassol@gmail.com>
2021-06-10 14:30:51 -0700

    New unit tests

---
+++ tests/testthat/test-02-SYSargs2.R
@@ -0,0 +1,77 @@
+## It will check the SYSargs2 class and methods
+library(systemPipeR)
+library(systemPipeRdata)
+
+genWorkenvir("rnaseq", mydirname = file.path(tempdir(), "rnaseq"))
+setwd(file.path(tempdir(), "rnaseq"))
+
+test_that("check_SYSargs2_test", {
+    ## build instance 
+    ## loadWorkflow() // renderWF()
+    dir_path <- system.file("extdata/cwl/test/", package = "systemPipeR")
+    dir.create("param/docopt.R/test", recursive = TRUE)
+    file.copy(system.file("extdata/docopt.R/test/test.doc.R", package = "systemPipeR"), to = "param/docopt.R/test", recursive = TRUE)
+    args <- loadWorkflow(targets = NULL, wf_file = "test.cwl",
+                         input_file = "test.yml", dir_path = dir_path)
+    args <- renderWF(args)
+    ## Methods
+    expect_type(names(args), "character")
+    expect_type(targets(args), "list")
+    expect_type(targetsheader(args), "list")
+    expect_type(modules(args), "character")
+    expect_type(wf(args), "list")
+    expect_type(clt(args), "list")
+    expect_type(yamlinput(args), "list")
+    expect_type(cmdlist(args), "list")
+    expect_type(input(args), "list")
+    expect_type(output(args), "list")
+    expect_type(cwlfiles(args), "list")
+    expect_type(inputvars(args), "list")
+    expect_type(cmdToCwl(args), "list")
+    expect_s4_class(args, "SYSargs2")
+    ## runCommandline() //check.output()
+    args <- runCommandline(args=args, force=TRUE)
+    #args2 <- runCommandline(args=args, dir = TRUE, force=TRUE)
+    out <- check.output(args)
+    expect_setequal(out$Existing_Files, "1")
+    ## symLink2bam()
+    symLink2bam(sysargs=args, command="ln -s", htmldir=c(tempdir(), "/rnaseq/somedir/"), 
+                ext=c(".bam", ".bai"), urlbase="http://myserver.edu/~username/", 
+                urlfile="IGVurl.txt")
+    expect_true(file.exists("somedir/.bam"))
+    ## readComp()
+    targetspath <- system.file("extdata", "targets.txt", package="systemPipeR")
+    cmp <- readComp(targetspath, format="vector", delim="-")
+    expect_type(cmp, "list")
+    expect_error(readComp(args, format="vector", delim="-"))
+})
+
+
+
+# test_that("check_SYSargs2", {
+#     ## build instance
+#     ## loadWorkflow() // renderWF()
+#     targets <- system.file("extdata", "targets.txt", package = "systemPipeR")
+#     dir_path <- system.file("extdata/cwl/rsubread/rsubread-se", package = "systemPipeR")
+#     args <- loadWorkflow(targets = targets, wf_file = "rsubread-mapping-se.cwl",
+#                              input_file = "rsubread-mapping-se.yml", dir_path = dir_path)
+#     args <- renderWF(args, inputvars = c(FileName = "_FASTQ_PATH1_", SampleName = "_SampleName_"))
+#     args <- args[1:2]
+#     expect_s4_class(args, "SYSargs2")
+#     ## build index
+#     dir_path <- system.file("extdata/cwl/rsubread/rsubread-idx", package = "systemPipeR")
+#     idx <- loadWorkflow(targets = NULL, wf_file = "rsubread-index.cwl", input_file = "rsubread-index.yml",
+#                         dir_path = dir_path)
+#     idx <- renderWF(idx)
+#     runCommandline(args = idx, make_bam = FALSE)
+#     ## Run alignment
+#     ## runCommandline() //check.output()
+#     args <- runCommandline(args=args)
+#     out <- check.output(args)
+#     expect_type(out, "logical")
+#     # symLink2bam()
+#     symLink2bam(sysargs=args, command="ln -s", htmldir=c(tempdir(), "/rnaseq/somedir/"),
+#                 ext=c(".bam", ".bai"), urlbase="http://myserver.edu/~username/",
+#                 urlfile="IGVurl.txt")
+#     expect_true(file.exists("somedir/M1A.bam"))
+# })
---
+++ tests/testthat/test-03-SYSargsList.R
@@ -0,0 +1,44 @@
+## It will check the SYSargs2 class and methods
+library(systemPipeR)
+setwd(file.path(tempdir(), "rnaseq"))
+
+test_that("check_SYSargsList_test", {
+    ## build instance 
+    dir_path <- system.file("extdata/cwl/example", package="systemPipeR")
+    sal <- SPRproject()
+    targetspath <- system.file("extdata/cwl/example/targets_example.txt", package="systemPipeR")
+    ## appendStep <-  //SYSargsList() // cmdlist() // length(sal)
+    appendStep(sal) <- SYSargsList(targets=targetspath, 
+                                   wf_file="example.cwl", input_file="example.yml", dir_path = dir_path, 
+                                   inputvars = c(Message = "_STRING_", SampleName = "_SAMPLE_"))
+    ## Methods
+    expect_type(names(sal), "character")
+    expect_type(sprconfig(sal), "list")
+    expect_type(length(sal), "integer")
+    expect_type(stepsWF(sal), "list")
+    expect_type(statusWF(sal), "list")
+    expect_type(dependency(sal), "list")
+    expect_type(projectWF(sal), "list")
+    expect_type(targetsWF(sal), "list")
+    expect_type(SEobj(sal), "list")
+    expect_type(outfiles(sal), "list")
+    expect_type(cmdlist(sal), "list")
+    expect_type(sysargslist(sal), "list")
+    ## Class
+    expect_s4_class(sal, "SYSargsList")
+    expect_length(sal, 1)
+    expect_length(cmdlist(sal)[[1]], 3)
+    ##runWF()
+   # sal <- runWF(sal)
+   # expect_setequal(statusWF(sal)[[1]][[1]], "DONE")
+   # check <- check.output(sal)
+   # expect_setequal(check$Step_1$Existing_Files, "1")
+   # 
+   ## replacement methods
+   expect_warning(appendStep(sal) <- sal)
+   renameStep(sal, 2) <- "newStep"
+   ## `+` method
+   sal <- sal[1] + sal[2] 
+   expect_length(sal, 2)
+})
+
