cf604fcfe64724d2286065cffa3bbcaa0b506239
dcassol <danicassol@gmail.com>
2021-11-03 00:53:10 +0000

    Bot update cwl description files

--- inst/extdata/docopt.R/preprocessReads/preprocessReads.doc.R
+++ inst/extdata/docopt.R/preprocessReads/preprocessReads.doc.R
@@ -4,10 +4,10 @@ Usage:
       preprocessReads_se.doc.R [--FileName1 <file>] [--FileName2 <file>] [--outfile1 <file>] [--outfile2 <file>] [--Fct <string>] [--batchsize <int>] [--overwrite <logical>] [--compress <logical>]
 
 Options:
-    --FileName1 <file> targets file path.
-    --FileName2 <file> targets file path.
-    --outfile1 <file> outfile file path.
-    --outfile2 <file> outfile file path.
+    --FileName1 <file> FileName1 file path.
+    --FileName2 <file> FileName2 file path.
+    --outfile1 <file> outfile1 file path.
+    --outfile2 <file> outfile2 file path.
     --Fct <string> character string of custom read preprocessing function call where both the input and output needs to be an object of class ShortReadQ. The name of the input ShortReadQ object needs to be fq.
     --batchsize <int> Number of reads to process in each iteration by the internally used FastqStreamer function.
     --overwrite <logical> If TRUE existing file will be overwritten.
