21bc28cfef07b1232415af89961f75b65630700d
dcassol <danicassol@gmail.com>
2022-04-13 17:09:40 -0700

    riboseq; build_check

--- R/riboseq.R
+++ R/riboseq.R
@@ -1183,19 +1183,19 @@ trimbatch <- function(fq, pattern, internalmatch=FALSE, minpatternlength=8,
     ## Iterative trimming based on regular expression
     for(j in seq(along=minpatternlength:(nchar(pattern)))-1) {
         mypattern <- paste(substring(pattern, 1, nchar(pattern)-j), regpart, sep="")
-        d <- DNAStringSet(gsub(mypattern, "", sread(fq)))
+        d <- Biostrings::DNAStringSet(gsub(mypattern, "", sread(fq)))
         q <- Biostrings::BStringSet(Biostrings::quality(Biostrings::quality(fq)), 1, width(d))
         fq <- ShortRead::ShortReadQ(sread=d, quality=q, id=id(fq))
-        processedreads <- append(processedreads, fq[width(fq) < mylength])
+        processedreads <- BiocGenerics::append(processedreads, fq[width(fq) < mylength])
         fq <- fq[width(fq) >= mylength]
     }
-    processedreads <- append(processedreads, fq)
+    processedreads <- BiocGenerics::append(processedreads, fq)
     processedreads <- processedreads[width(processedreads)>=minreadlength & width(processedreads)<=maxreadlength] # Read length filter
     filter1 <- ShortRead::nFilter(threshold=Nnumber) # keep only reads with <= x Ns
     filter2 <- ShortRead::polynFilter(threshold=polyhomo, nuc=c("A", "C", "T", "G")) # remove reads with x or more of the same nucleotide
     filter <- ShortRead::compose(filter1, filter2) # Combine filters into one
     processedreads[filter(processedreads)]
-}
+    }
 ## Usage:
-# iterTrim <- "trimbatch(fq, pattern="ACACGTCT", minpatternlength=6, Nnumber=1, polyhomo=20, minreadlength=16) 
-# preprocessReads(args=args, Fct="iterTrim", batchsize=100000, overwrite=TRUE, compress=TRUE)
+# iterTrim <- trimbatch(fq, pattern="ACACGTCT", minpatternlength=6, Nnumber=1, polyhomo=20, minreadlength=16, maxreadlength = 101)
+# preprocessReads(args=args, Fct=iterTrim, batchsize=100000, overwrite=TRUE, compress=TRUE)
